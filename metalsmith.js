f5f1310592437476307f274499611a6983d449a6
dom-o <dom-o@users.noreply.github.com>
2023-07-30 06:08:18 -0400

    Add metalsmith-modified package; stop using haps; change sitewide date month format from long month name to short month name.

--- metalsmith.js
+++ metalsmith.js
@@ -4,9 +4,10 @@ const Metalsmith = require('metalsmith'),
   rootpath = require('metalsmith-rootpath'),
   layouts = require('@metalsmith/layouts'),
   imageAspectRatio = require('metalsmith-image-aspect-ratio'),
+  updated = require('metalsmith-updated')
 
   feed = require('./plugins/feed'),
-  haps = require('./plugins/haps'),
+  // haps = require('./plugins/haps'),
   imgToPicture = require('./plugins/imgToPicture'),
 
   debug = require('metalsmith-debug')
@@ -18,22 +19,28 @@ Metalsmith(__dirname)
       url: 'https://mostbiggestegg.com/',
       author: 'egghorn',
       title:'mostbiggestegg.com',
-      description: 'egghorn website.'
+      description: 'egghorn website.',
+      build_date: new Date() 
     },
   })
   .use(metadata({
     'pages': 'src/pagelist.yaml',
     'projs': 'src/projlist.yaml',
-    'haps-events': 'src/hapslist.yaml'
+    // 'haps-events': 'src/hapslist.yaml'
   }))
-  .use(haps())
+  // .use(haps())
   .use(markdown())
+  .use(updated({
+    'updatedFile':'file_last_modified_log.json'
+  }))
   .use(rootpath())
   .use(layouts({
+    default: 'page.njk',
+    pattern: ['**/*.html'],
     engineOptions: {
       filters: {
         UTCdate: string => (new Date(string)),
-        blogDate: string => (new Date(string).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })),
+        blogDate: string => (new Date(string).toLocaleString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })),
         match: (list, prop, val) => list.find(a => a[prop]==val),
         filter: (list, prop, val) => list.filter(a => a[prop] == val),
       }
